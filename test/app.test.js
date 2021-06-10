a4280a727a65e457b0ba9acc8cda4ea26b28932d
ChadH28 <chadhoosain@gmail.com>
2021-06-10 16:36:41 +0200

    finished the tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,104 @@
+const app = require('../index')
 const knex = require('../db/knex')
+const request = require('supertest');
+const { expect } = require('chai');
+const fixtures = require('./fixtures')
 
 describe('CRUD stickers', () => {
-    before( (done) => {
-        // run migrations
-        // run seeds
-        knex.migrate.latest()
+    before(async function () {
+        // run migrations    
+        await knex.migrate.latest()
             .then(() => {
-                return knex.seed.run()
+                // run seeds
+                return knex.seed.run();
             })
-            .then(() => done() )
-    })
-    it('works', function () {
-        console.log('working 100');
-    })
-})
+            .then(() => done());
+    });
+
+    it('Listing all records', async function () {
+        request(app)
+            .get('/api/stickers')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('array')
+            })
+    });
+});
+
+describe('ListRecords()', () => {
+    it('Listing all records', async function () {
+        request(app)
+            .get('/api/stickers')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('array')
+            })
+    });
+});
+
+
+describe('ListOneRecord()', () => {
+    it('Listing one record', async function () {
+        request(app)
+            .get('/api/stickers/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object')
+                expect(response.body).to.deep.equal(fixtures.stickers[0])
+            })
+    });
+});
+
+describe('createRecord()', () => {
+    it('created a record', async function (done) {
+        request(app)
+            .post('/api/stickers')
+            .send(fixtures.sticker)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object')
+                fixtures.sticker.id = response.body.id
+                expect(response.body).to.deep.equal(fixtures.sticker)
+            })
+            done()
+    });
+});
+
+
+describe('editOneRecord()', () => {
+    it('editing one record', async function () {
+        fixtures.sticker.rating = 5;
+        request(app)
+            .put('/api/stickers/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object')
+                expect(response.body).to.deep.equal(fixtures.sticker)
+            })
+    });
+});
+
+describe('deleteRecord()', () => {
+    it('editing one record', async function () {
+        request(app)
+            .delete('/api/stickers/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('object')
+                expect(response.body).to.deep.equal(fixtures.sticker)
+                deleted: true
+            })
+    });
+});
---
+++ test/fixtures.js
@@ -0,0 +1,63 @@
+ const stickers = [
+    {
+      "id": 1,
+      "title": "Man U",
+      "description": "Man U logo",
+      "rating": 5,
+      "url": "https://stickercommunity.com/uploads/icon/1622835812_icon.png"
+    },
+    {
+      "id": 2,
+      "title": "funny",
+      "description": "funny logo",
+      "rating": 8,
+      "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-56ch3ay-sticker0.webp"
+    },
+    {
+      "id": 3,
+      "title": "thelook",
+      "description": "thelook logo",
+      "rating": 5,
+      "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-56kiwxm-sticker19.webp"
+    },
+    {
+      "id": 4,
+      "title": "face",
+      "description": "face logo",
+      "rating": 8,
+      "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-56jak79-sticker9.webp"
+    },
+    {
+      "id": 5,
+      "title": "sponge",
+      "description": "sponge logo",
+      "rating": 4,
+      "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-56g5mu3-sticker10.webp"
+    },
+    {
+      "id": 6,
+      "title": "train",
+      "description": "train logo",
+      "rating": 9,
+      "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-568lgv6-sticker22.webp"
+    },
+    {
+      "id": 7,
+      "title": "thenote",
+      "description": null,
+      "rating": null,
+      "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-56kiwxm-sticker19.webp"
+    }
+  ]
+
+  const sticker = {
+    "title": "sleeep",
+    "description": null,
+    "rating": null,
+    "url": "https://stickercommunity.com/uploads/main/03-06-2021-12-22-56kiwxm-sticker19.webp"
+  }
+
+  module.exports = {
+      stickers,
+      sticker
+  }
